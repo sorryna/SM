52a8e3d43b8d3da84dec0440bc6fff1b263bff9c
Sikarin <so.sikarin@perenterprise.com>
2018-09-15 15:30:40 +0700

    implement quest II
    Signed-off-by: Sikarin <so.sikarin@perenterprise.com>

--- change/Program.cs
+++ change/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Linq;
 
 namespace change
 {
     public class Program
     {
 
-        public int[] exchange(int input)
+        public static int[] exchange(int input)
         {
             int[] t = new int[9];
             int[] Banknotes = { 1000, 500, 100, 50, 20, 10, 5, 2, 1 };
@@ -23,18 +24,20 @@ namespace change
             return t;
         }
 
-        public int[] what(int dd)
+        public static int[] what(int dd)
         {
-            int[] a = new int[5];
+            int[] a = new int[0];
             if (dd % 2 == 1)
             {
                 var x = dd / 2;
+                a = new int[2];
                 a[0] = x;
                 a[1] = x + 1;
             }
             else if (dd % 3 == 0)
             {
                 var x = dd / 3;
+                a = new int[3];
                 a[0] = x - 1;
                 a[1] = x;
                 a[2] = x + 1;
@@ -42,25 +45,32 @@ namespace change
             else if (dd % 4 == 2)
             {
                 var x = dd / 4;
+                a = new int[4];
                 a[0] = x - 1;
                 a[1] = x;
-                a[3] = x + 1;
-                a[4] = x + 2;
+                a[2] = x + 1;
+                a[3] = x + 2;
             }
             else if (dd % 5 == 0)
             {
-
+                var x = dd / 4;
+                a = new int[5];
+                a[0] = x - 2;
+                a[1] = x - 1;
+                a[2] = x;
+                a[3] = x + 1;
+                a[4] = x + 2;
             }
             return a;
         }
 
         static void Main(string[] args)
         {
-            // var a = change(int.Parse(Console.ReadLine()));
-            // foreach (var item in a)
-            // {
-            //    Console.WriteLine(item); 
-            // }            
+            var a = what(int.Parse(Console.ReadLine()));
+            foreach (var item in a)
+            {
+               Console.WriteLine(item); 
+            }            
         }
     }
 }
